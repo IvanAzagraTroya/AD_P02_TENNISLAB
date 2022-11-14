b97eb55840b945d32e563082053d6e442f07a860
Iván Azagra <ivanazagratroya@gmail.com>
2022-11-14 20:07:52 +0100

    Parser de estados de pedido

--- src/main/kotlin/models/enums/TipoProducto.kt
+++ src/main/kotlin/models/enums/TipoProducto.kt
@@ -4,6 +4,7 @@ package models.enums
  * @author Iván Azagra Troya
  * Enumerador de los tipos de productos que puede haber junto a un
  * companion object que actúa como parser
+ * @throws IllegalArgumentException si el tipo de producto no existe lanza esta excepción
  */
 enum class TipoProducto(value: String){
     RAQUETAS("RAQUETAS"),
