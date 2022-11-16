58786c46ba002f2fb3589249f121a328ef9316b1
Iván Azagra <ivanazagratroya@gmail.com>
2022-11-16 21:11:55 +0100

    repo clientes

--- src/main/kotlin/repositories/clientes/ClientesRepositoryImpl.kt
+++ src/main/kotlin/repositories/clientes/ClientesRepositoryImpl.kt
@@ -1,22 +1,21 @@
 package repositories.clientes
 
-/*
-import entities.UsersDao
-import mappers.fromUsersDaoToUser
+import entities.UserDao
+import mappers.fromUserDaoToUser
 import models.User
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
 class ClientesRepositoryImpl(
-    private val clientesDao: UUIDEntityClass<UsersDao>,
+    private val clientesDao: UUIDEntityClass<UserDao>,
 ): IClientesRepository {
     override fun findAll(): List<User> = transaction {
-        clientesDao.all().map { it.fromUsersDaoToUser() }
+        clientesDao.all().map { it.fromUserDaoToUser() }
     }
 
     override fun findById(id: UUID): User? = transaction {
-        clientesDao.findById(id)?.fromUsersDaoToUser()
+        clientesDao.findById(id)?.fromUserDaoToUser()
     }
 
     override fun create(entity: User): User = transaction {
@@ -38,23 +37,23 @@ class ClientesRepositoryImpl(
             apellido = entity.apellido
             telefono = entity.telefono
             email = entity.email
-            contraseña = entity.contraseña
-        }.fromUsersDaoToUser()
+            password = entity.password
+        }.fromUserDaoToUser()
     }
 
     override fun readAll(): List<User> = transaction{
-        clientesDao.all().map { it.fromUsersDaoToUser() }
+        clientesDao.all().map { it.fromUserDaoToUser() }
     }
 
 
-    private fun update(entity: User, existe: UsersDao): User {
+    private fun update(entity: User, existe: UserDao): User {
         return existe.apply {
             nombre = entity.nombre
             apellido = entity.apellido
             telefono = entity.telefono
             email = entity.email
-            contraseña = entity.contraseña
-        }.fromUsersDaoToUser()
+            password = entity.password
+        }.fromUserDaoToUser()
     }
 
     override fun delete(entity: User): Boolean = transaction{
