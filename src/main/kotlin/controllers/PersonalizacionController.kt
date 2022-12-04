e5d2ad3e582a2d285988bb3bda68dc42e8d5bb01
Iván Azagra Troya <67174666+IvanAzagraTroya@users.noreply.github.com>
2022-12-04 22:45:24 +0100

    Update PersonalizacionController.kt

--- src/main/kotlin/controllers/PersonalizacionController.kt
+++ src/main/kotlin/controllers/PersonalizacionController.kt
@@ -11,47 +11,19 @@ object PersonalizacionController {
 
     suspend fun findAllPersonalizaciones(): String {
         return service.getAllPersonalizaciones().toString()
-        /*
-        val result = GsonBuilder().setPrettyPrinting().create()
-            .toJson(service.getAllPersonalizaciones())
-            ?: "Error at PersonalizacionController.findAllPersonalizaciones"
-        return "prueba"//generateRespuesta(result, "Error at PersonalizacionController.findAllPersonalizaciones")
-
-         */
     }
 
     suspend fun getPersonalizacionById(id: UUID): String {
         return service.getPersonalizacionById(id)?.toJSON() ?: "Personalizacion with id $id not found."
-        /*
-        val result = GsonBuilder().setPrettyPrinting().create()
-            .toJson(service.getPersonalizacionById(id))
-            ?: "Personalizacion with id $id not found."
-        return "prueba"//generateRespuesta(result, "Personalizacion with id $id not found.")
-
-         */
     }
 
     suspend fun insertPersonalizacion(dto: PersonalizacionDTO): String {
         return service.createPersonalizacion(dto).toJSON()
-        /*
-        val result = GsonBuilder().setPrettyPrinting().create()
-            .toJson(service.createPersonalizacion(dto))
-            ?: "Could not insert Personalizacion with id ${dto.id}"
-        return "prueba"//generateRespuesta(result, "Could not insert Personalizacion with id ${dto.id}")
-
-         */
     }
 
     suspend fun deletePersonalizacion(dto: PersonalizacionDTO): String {
         val result = service.deletePersonalizacion(dto)
         return if (result) dto.toJSON()
         else "Could not delete Personalizacion with id ${dto.id}"
-        /*
-        val result =GsonBuilder().setPrettyPrinting().create()
-            .toJson(service.deletePersonalizacion(dto))
-            ?: "Could not delete Personalizacion with id ${dto.id}"
-        return "prueba"//generateRespuesta(result, "Could not delete Personalizacion with id ${dto.id}")
-
-         */
     }
 }
